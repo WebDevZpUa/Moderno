45301a9f59a4d476f1599950db38dcb88084cddf
WebDevZpUa <your_email@whatever.com>
2022-09-06 20:37:56 +0300

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
   let mixer = mixitup(".products__inner-box");
+  $(".rate-star").rateYo({
+    rating: 5,
+    readOnly: true,
+    rating: 4.2,
+    starWidth: "12px",
+  });
+  $(".product-slider__inner").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false,
+  });
 });
